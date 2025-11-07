329fa6ca1d2102e569f35d4e699e54a3ac8fc69b
Eley98 <gabdullinaelina310@gmail.com>
2025-11-07 12:41:17 +0500

    process

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-Element.style.property = new style
+const btn = document.getElementById('Btn');
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+
+btn.addEventListener('click', () => {
+ 
+  if (img1.style.display === 'block') {
+    img1.style.display = 'none';
+    img2.style.display = 'block';
+
+    document.body.classList.add('dark-theme');
+  } else {
+   
+    img1.style.display = 'block';
+    img2.style.display = 'none';
+
+
+    document.body.classList.remove('dark-theme');
+  }
+});
+
+
