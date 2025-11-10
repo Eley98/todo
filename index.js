e22c1c5929c0afd47ee329b4711487c28793c324
Eley98 <gabdullinaelina310@gmail.com>
2025-11-10 23:52:41 +0500

    process

--- index.js
+++ index.js
@@ -3,21 +3,44 @@ const img1 = document.getElementById('img1');
 const img2 = document.getElementById('img2');
 
 btn.addEventListener('click', () => {
- 
   if (img1.style.display === 'block') {
     img1.style.display = 'none';
     img2.style.display = 'block';
 
     document.body.classList.add('dark-theme');
   } else {
-   
     img1.style.display = 'block';
     img2.style.display = 'none';
-
 
     document.body.classList.remove('dark-theme');
   }
 });
+// Получаем элементы
+const openBtn = document.getElementById('openModalBtn');
+const modal = document.getElementById('modal');
+const overlay = document.getElementById('overlay');
+const cancelBtn = document.getElementById('cancelBtn');
+const applyBtn = document.getElementById('applyBtn');
+
+// Функция открыть модальное
+function openModal() {
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+
+// Функция закрыть модальное
+function closeModal() {
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
+// Обработчики
+openBtn.addEventListener('click', openModal);
+cancelBtn.addEventListener('click', closeModal);
+applyBtn.addEventListener('click', closeModal);
+
+// Можно закрывать при клике вне окна
+overlay.addEventListener('click', closeModal);
 
 
 
